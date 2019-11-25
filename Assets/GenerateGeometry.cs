6a87ae4fade4966ecc4c7e2ef0fe8ba3677b7cef
alessio Acampora <acampora199@gmail.com>
2019-11-25 17:42:10 +0000

    added piramid shape

--- Assets/GenerateGeometry.cs
+++ Assets/GenerateGeometry.cs
@@ -19,9 +19,11 @@ public class GenerateGeometry : MonoBehaviour
         //Create vertices, we must have these
         Vector3[] vertices =
         {
-            new Vector3(0.0f,0.0f,0.0f),
-            new Vector3(0.5f,1.0f,0.0f),
-            new Vector3(1.0f,0.0f,0.0f)
+            new Vector3(0.0f,0.0f,0.0f),//0
+            new Vector3(1.0f,0.0f,0.0f),//1
+            new Vector3(1.0f,0.0f,1.0f),//2
+            new Vector3(0.0f,0.0f,1.0f),//3
+            new Vector3(0.5f,1.0f,0.5f) //4
         };
 
         //Normals and other elements are options but will be required
@@ -30,11 +32,31 @@ public class GenerateGeometry : MonoBehaviour
         {
             new Vector3(0.0f,0.0f,-1.0f),
             new Vector3(0.0f,0.0f,-1.0f),
+            new Vector3(0.0f,0.0f,-1.0f),
+            new Vector3(0.0f,0.0f,-1.0f),
             new Vector3(0.0f,0.0f,-1.0f)
         };
 
         //Indices, called triangles in Unity, these are indices into the Vertex array above
-        int[] triangles = { 0, 1, 2 };
+        int[] triangles = { 
+            0, 1, 2, //left triangle of base
+            2, 3, 0, //right triangle of base
+            0, 4, 1, //face a
+            1, 4, 2, //face b
+            2, 4 ,3, //face c
+            3, 4, 0  //face d
+        };
+
+        // piramid vertices 
+        //Indices, called triangles in Unity, these are indices into the Vertex array above
+        //int[] triangles = {
+        //    0, 1, 2, //left triangle of base
+        //    2, 3, 0, //right triangle of base
+        //    0, 4, 1, //face a
+        //    1, 4, 2, //face b
+        //    2, 4 ,3, //face c
+        //    3, 4, 0  //face d
+        //};
 
         //Assign all the values in the mesh
         mesh.vertices = vertices;
